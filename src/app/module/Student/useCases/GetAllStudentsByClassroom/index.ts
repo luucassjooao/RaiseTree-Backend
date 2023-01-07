bdf77ee4b08c64bff3e1acb4ca4a5ee653e5e28a
lucasjoaoo <mainexcalibur@protonmail.com>
2023-01-07 17:57:34 -0300

    removing the redis cache when the teacher searches for rooms

--- src/app/module/Student/useCases/GetAllStudentsByClassroom/index.ts
+++ src/app/module/Student/useCases/GetAllStudentsByClassroom/index.ts
@@ -1,5 +1,3 @@
-import { KeyRedisAllClassroomsOfTeacherInfosStudent } from '../../../../../lib/cache/keys/student';
-import redis from '../../../../../lib/cache/redis';
 import AppError from '../../../../error';
 import StudentRepository from '../../repositories/implementation/StudentRepository';
 
@@ -8,15 +6,15 @@ export default async function GetAllStudentsByClassroom(
   teacherClassroom: string[],
 ) {
   if (!teacherClassroom.includes(classroom)) throw new AppError('Voce nao pode ver os alunos dessa sala!');
-  const keyClassroom = KeyRedisAllClassroomsOfTeacherInfosStudent(classroom);
+  // const keyClassroom = KeyRedisAllClassroomsOfTeacherInfosStudent(classroom);
 
-  const cachedClassroom = await redis.get(keyClassroom);
-  if (cachedClassroom) {
-    return JSON.parse(cachedClassroom);
-  }
+  // const cachedClassroom = await redis.get(keyClassroom);
+  // if (cachedClassroom) {
+  //   return JSON.parse(cachedClassroom);
+  // }
 
   const getStudents = await StudentRepository.getAllStudentsByClassroom(classroom);
-  await redis.set(keyClassroom, JSON.stringify(getStudents), 'EX', 600);
+  // await redis.set(keyClassroom, JSON.stringify(getStudents), 'EX', 600);
 
   return getStudents;
 }
