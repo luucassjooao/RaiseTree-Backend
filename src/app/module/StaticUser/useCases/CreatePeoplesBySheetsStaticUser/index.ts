abd3463a6e2312a5b4aaf80610f6cad8514399a0
lucasjoaoo <mainexcalibur@protonmail.com>
2023-02-01 22:08:25 -0300

    fix registration for sheets

--- src/app/module/StaticUser/useCases/CreatePeoplesBySheetsStaticUser/index.ts
+++ src/app/module/StaticUser/useCases/CreatePeoplesBySheetsStaticUser/index.ts
@@ -31,22 +31,23 @@ export default async function CreatePeoplesBySheetsStaticUser(
 
   const peoplesAdding: Prisma.Prisma__StaticUserClient<TStaticUser, never>[] = [];
   const teacherCreated: SendMailForTeacher[] = [];
+  const cpfOnRow: string[] = [];
 
   // eslint-disable-next-line no-restricted-syntax
   for await (const acRow of rows.keys()) {
     const row = rows[acRow];
 
-    if (typeOfPeoples === 'student' && row.Cpf === undefined) throw new AppError('Na sua planilha não tem o cabeçalho CPF!');
+    if (typeOfPeoples === 'student' && row.CPF === undefined) throw new AppError('Na sua planilha não tem o cabeçalho CPF!');
     if (row.Nome === undefined) throw new AppError('Na sua planilha não tem o cabeçalho Nome!');
     if (row.Sala === undefined) throw new AppError('Na sua planilha não tem o cabeçalho Sala!');
 
-    const findCpfInStaticUser = await StaticUserRepository.findCode(row.Cpf);
+    const findCpfInStaticUser = await StaticUserRepository.findCode(row.CPF);
     if (
       (typeOfPeoples === 'student' && findCpfInStaticUser)
       && findCpfInStaticUser.organizationId !== organizationId
-    ) throw new AppError(`O CPF: ${row.Cpf} já está cadastrado, verifique com o estudante: ${row.Nome}!`);
+    ) throw new AppError(`O CPF: ${row.CPF} já está cadastrado, verifique com o estudante: ${row.Nome}!`);
 
-    const findCpfInUser = await UserRepository.findCode(row.Cpf);
+    const findCpfInUser = await UserRepository.findCode(row.CPF);
     if (
       (typeOfPeoples === 'student' && findCpfInUser)
       && findCpfInUser.organizationId === organizationId
@@ -55,13 +56,16 @@ export default async function CreatePeoplesBySheetsStaticUser(
     const findClassRoomOfPeople = classroomsOfOrganization
       .findIndex((sala) => sala === row.Sala);
 
+    if (cpfOnRow.includes(row.CPF)) throw new AppError(`O CPF ${row.CPF}, está duplicado`);
+
     if (typeOfPeoples === 'student' && (findClassRoomOfPeople !== -1)) {
+      cpfOnRow.push(row.CPF);
       peoplesAdding.push(
         prismaClient.staticUser.create({
           data: {
             name: row.Nome,
             classroom: [`${prefixClassroom} | ${row.Sala}`],
-            code: row.Cpf,
+            code: row.CPF,
             type: 'student',
             organizationId,
           },
